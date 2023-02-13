d998d1102119e1b5ed12df6d3df906ddff22419d
BnnQ <1v25fa94@gmail.com>
2023-02-13 19:32:32 +0200

    Added quest room logo image processor

---
+++ Services/QuestRoomLogoImageSaver.cs
@@ -0,0 +1,37 @@
+﻿using Homework.Services.Abstractions;
+
+namespace Homework.Services
+{
+    public class QuestRoomLogoImageSaver : IFormImageProcessor
+    {
+        private readonly IWebHostEnvironment environment;
+        private readonly IFileNameGenerator fileNameGenerator;
+
+        public QuestRoomLogoImageSaver(IWebHostEnvironment environment, IFileNameGenerator fileNameGenerator)
+        {
+            this.environment = environment;
+            this.fileNameGenerator = fileNameGenerator;
+        }
+
+        public string Process(IFormFile formImage)
+        {
+            string fileName = fileNameGenerator.GenerateFileName(baseFileName: Path.GetFileNameWithoutExtension(formImage.FileName),
+                                                                 fileNameExtension: Path.GetExtension(formImage.FileName));
+
+            const string rootDirectory = "media";
+            const string controllerDirectory = "QuestRoom";
+            const string logosDirectory = "Logos";
+            string logoWebRelativePath = Path.Combine(rootDirectory, controllerDirectory, logosDirectory, fileName);
+
+            string logoFullPath = Path.Combine(environment.WebRootPath, logoWebRelativePath);
+            using (FileStream fileStream = new(logoFullPath, FileMode.Create, FileAccess.Write))
+            {
+                using (Stream imageStream = formImage.OpenReadStream())
+                    imageStream.CopyTo(fileStream);
+            }
+
+            return logoWebRelativePath.Replace("\\", "/");
+        }
+
+    }
+}
