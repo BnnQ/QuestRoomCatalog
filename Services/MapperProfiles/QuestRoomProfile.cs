13a5ecb1f2991b44bd4c0384f3ffac98a34e0588
BnnQ <1v25fa94@gmail.com>
2023-02-13 19:33:17 +0200

    Added QuestRoom automapper's profile

---
+++ Services/MapperProfiles/QuestRoomProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Homework.Data.Entities;
+using Homework.Models;
+
+namespace Homework.Services.MapperProfiles
+{
+    public class QuestRoomProfile : Profile
+    {
+        public QuestRoomProfile() 
+        {
+            CreateMap<QuestRoom, QuestRoomDto>().ReverseMap();
+        }
+    }
+}
